ab912c8f72ec2ac94f1bc9cfc08fba4da8f222b5
ur2e <dbfl9711@gmail.com>
2021-01-31 02:43:12 +0900

    update pageView

--- timmyroom_jansori/lib/main.dart
+++ timmyroom_jansori/lib/main.dart
@@ -13,24 +13,8 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      darkTheme: ThemeData.dark(),
+      home: HomePage(),
     );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
   }
 }
