bc834190a8f5a66c4c58f1c87e589c92bf16703f
DESKTOP-R8PRJDL\sarah <ksmself@gmail.com>
2021-02-02 02:57:46 +0900

    flutter upgrade 전 commit

--- timmyroom_jansori/lib/page3.dart
+++ timmyroom_jansori/lib/page3.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'package:dotted_border/dotted_border.dart';
 
 
 class Page3 extends StatelessWidget{
@@ -21,15 +20,52 @@ class Page3 extends StatelessWidget{
   Widget popup(){
     return Center(
       child:
-        DottedBorder(
-          strokeWidth: 2,
-          dashPattern: [2, 2],
-          color: Color.fromRGBO(93, 136, 248, 0.8),
-          child:
-            Container(
-              width: 285,
-              height: 450,
-            ),
+          Container(
+            width: 285,
+            height: 450,
+            decoration:
+              BoxDecoration(
+                border: Border.all(
+                  width: 2,
+                  color: Color.fromRGBO(93, 136, 248, 0.8),
+                ),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+            child:
+              Container(
+                width: 255,
+                margin: EdgeInsets.all(15),
+                child:
+                  timeSetting(),
+              ),
+          ),
+    );
+  }
+
+  Widget timeSetting(){
+    return Container(
+      width: 255,
+      // height: 103.25,
+      /*
+      decoration: BoxDecoration(
+        border: Border.all(
+        width: 2,
+        color: Color.fromRGBO(93, 136, 248, 0.8),
+      )),
+      */
+      child:
+        Text(
+          "시간 설정",
+          style: TextStyle(
+            fontFamily: 'Roboto',
+            fontStyle: FontStyle.normal,
+            fontSize: 14,
+            fontWeight: FontWeight.w400,
+            color: Color.fromRGBO(4, 4, 7, 0.7),
+            height: 1.4,
+          ),
         ),
     );
   }
