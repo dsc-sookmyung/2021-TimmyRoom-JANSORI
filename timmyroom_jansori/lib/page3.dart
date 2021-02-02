6a30cf52c7266d4526c9e0857f5a1ed971fbabd4
DESKTOP-R8PRJDL\sarah <ksmself@gmail.com>
2021-02-03 04:41:07 +0900

    집중 타임 드롭다운 선택 후 표시 에러 미해결

--- timmyroom_jansori/lib/page3.dart
+++ timmyroom_jansori/lib/page3.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'year_month_day_time_picker.dart';
+// import 'year_month_day_time_picker.dart';
 
+class MainPage3 extends StatefulWidget{
+  Page3 createState()=> Page3();
+}
 
-
-class Page3 extends StatelessWidget{
+class Page3 extends State<MainPage3>{
 
   @override
   Widget build(BuildContext context){
@@ -25,13 +27,12 @@ class Page3 extends StatelessWidget{
       child:
           Container(
             width: 285,
-            height: 450,
+            height: 500,
             decoration:
               BoxDecoration(
                 border: Border.all(
                   width: 0.8,
                   color: Color.fromRGBO(93, 136, 248, 0.8),
-                  // color: Color.fromRGBO(4, 4, 7, 0.2),
                 ),
                 borderRadius: BorderRadius.all(
                   Radius.circular(5),
@@ -48,6 +49,7 @@ class Page3 extends StatelessWidget{
                       timeSetting(),
                       nameSetting(),
                       jansoriSetting(),
+                      submitButton(),
                     ],
                   ),
               ),
@@ -55,7 +57,33 @@ class Page3 extends StatelessWidget{
     );
   }
 
+  submit() {
+  }
+
+  Widget submitButton(){
+    return Column(
+      children: [
+        SizedBox(height: 10.0),
+        MaterialButton(
+          onPressed: submit,
+          color: Color.fromRGBO(93, 136, 248, 1),
+          textColor: Colors.white,
+          padding: EdgeInsets.symmetric(
+            horizontal: 94.0,
+            vertical: 10.0,
+          ),
+          child: Text(
+            'Submit',
+            style: TextStyle(fontSize: 20.0),
+          ),
+        ),
+      ],
+    );
+  }
+
   Widget timeSetting(){
+    int _value;
+
     return Container(
       width: 255,
       margin: EdgeInsets.fromLTRB(0, 0, 0, 20),
@@ -81,7 +109,110 @@ class Page3 extends StatelessWidget{
                         ),
                       ),
                   ),
-                  YearMonthPicker(),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child:
+                          Container(
+                              height: 20,
+                              // color: Colors.green,
+                              child: Container(
+                                child:
+                                  Text(
+                                    "집중 타임(시간)",
+                                    style: TextStyle(fontSize: 12),
+                                    textAlign: TextAlign.center,
+                                  ),
+                              ),
+                          ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child:
+                          Container(
+                              height: 20,
+                              // color: Colors.amber,
+                              child: Container(
+                                child:
+                                Text(
+                                  "휴식 타임(분)",
+                                  style: TextStyle(fontSize: 12),
+                                  textAlign: TextAlign.center,
+                                ),
+                              )
+                          ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        flex: 2,
+                        child:
+                          Container(
+                            height: 40,
+                            //color: Colors.amber,
+                            child:
+                              Row(
+                                children: [
+                                  DropdownButton(
+                                    value: _value,
+                                    items: [
+                                      DropdownMenuItem(
+                                        child: Text("0"),
+                                        value: 1,
+                                      ),
+                                      DropdownMenuItem(
+                                        child: Text("1"),
+                                        value: 2,
+                                      ),
+                                      DropdownMenuItem(
+                                          child: Text("2"),
+                                          value: 3
+                                      ),
+                                      DropdownMenuItem(
+                                          child: Text("3"),
+                                          value: 4
+                                      ),
+                                      DropdownMenuItem(
+                                          child: Text("4"),
+                                          value: 5
+                                      ),
+                                      DropdownMenuItem(
+                                          child: Text("5"),
+                                          value: 6
+                                      ),
+                                      DropdownMenuItem(
+                                          child: Text("6"),
+                                          value: 7
+                                      )
+                                    ],
+                                  onChanged: (value) {
+                                    setState(() {
+                                      _value = value;
+                                    });
+                                  },
+                                  //value: _value,
+                                  ),
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(10, 0, 5, 0),
+                                    child: Text("시간"),
+                                  ),
+                                ],
+                              ),
+                          ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child:
+                          Container(
+                            height: 40,
+                            color: Colors.green,
+                          ),
+                      ),
+                    ],
+                  ),
                 ],
                ),
           ),
@@ -156,16 +287,33 @@ class Page3 extends StatelessWidget{
             ),
         ),
         Container(
-          height: 103.5,
+          margin: EdgeInsets.fromLTRB(0, 0, 0, 5),
+          height: 130,
           decoration: BoxDecoration(
             border: Border.all(
-              color: Color.fromRGBO(93, 136, 248, 0.8),
+              color: Color.fromRGBO(4, 4, 7, 0.35),
             ),
+            borderRadius: BorderRadius.all(Radius.circular(3)),
           ),
           child:
-            ListView(),
+            jansoriList(),
         ),
       ],
     );
   }
+
+  Widget jansoriList () {
+    List<String> items = List.generate(10, (i) => 'jansori #$i');
+    return ListView.builder(
+      itemCount: items.length,
+      itemBuilder: (context, i) {
+        return ListTile(
+            title: Text(items[i]),
+            subtitle: Text('this is jansori $i')
+        );
+      },
+    );
+  }
+
+
 }
